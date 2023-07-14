ad5e57f00f7cddc81f5ea0a9722f42b4836ba5d4
10decoders <10decoders@DESKTOP-1TO0V7A>
2023-07-14 18:11:34 +0530

    Add project files.

---
+++ ConsoleToWebAPI/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from new Web API app");
+                });
+            });
+        }
+    }
+}
